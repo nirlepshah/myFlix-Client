37f54a505bba2c8e5e0210ab7480e01f02903b6a
nirlepshah <nirlepshah@gmail.com>
2022-01-11 20:50:34 -0500

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import axios
+ from 'axios';
 export class MainView extends React.Component {
     constructor(){
         super(); //Adding state to the MainView Component
         this.state = {
             // Movie Array State
-            movies:[
-                { _id:"61c9550c8ecc8b1b59e89555", Title:"The Shawshank Redemption", Description:"Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", ImagePath: "../img/img1.jpg", Genre:"Drama", Director:"Frank Darabont"},
-                { _id:"61c955188ecc8b1b59e89556", Title:"The Godfather", Description:"The Godfather follows Vito Corleone, Don of the Corleone family, as he passes the mantel to his unwilling son, Michael.", ImagePath:"../img/img2.jpg",Genre:"Crime", Director:"Francis Ford Coppola"},
-                { _id:"61c955248ecc8b1b59e89557", Title:"Spirited Away", Description:"During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.", ImagePath:"../img/img3.jpg",Genre:"Animation", Director:"Hayao Miyazaki"}
-            ],
-            selectedMovie: null // Second State
+            movies:[],
+                
+           selectedMovie: null // Second State
         }
         
+    }
+    componentDidMount(){
+axios.get('https://mymovieapp08.herokuapp.com/movies')
+.then(response => {
+  this.setState({
+    movies: response.data
+  })
+})
+.catch(error => {
+  console.log(error)
+})
     }
     setSelectedMovie(newSelectedMovie) {
         this.setState({
@@ -21,11 +31,9 @@ export class MainView extends React.Component {
         });
       }
   render() {
-    const { movies, selectedMovie } = this.state;
+   // const { movies, selectedMovie } = this.state;
+   
 
-    
-
-  if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
 
   return (
     <div className="main-view">
