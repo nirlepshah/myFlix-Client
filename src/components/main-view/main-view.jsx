83cdab6a7cdd3079cf7f7ba3d08a18dd0d92268d
nirlepshah <nirlepshah@gmail.com>
2022-01-12 23:21:33 -0500

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { RegistrationView } from '../registration-view/registration-view';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
-import axios
- from 'axios';
+
+import axios from 'axios';
+
 export class MainView extends React.Component {
     constructor(){
         super(); //Adding state to the MainView Component
         this.state = {
             // Movie Array State
             movies:[],
-                
-           selectedMovie: null // Second State
+            selectedMovie: null, // Second State
+            user: null
         }
         
     }
+    // Use of Axios to fetch movies from API server's /movies endpoint 
     componentDidMount(){
 axios.get('https://mymovieapp08.herokuapp.com/movies')
 .then(response => {
@@ -25,16 +29,34 @@ axios.get('https://mymovieapp08.herokuapp.com/movies')
   console.log(error)
 })
     }
+    
+    // When movie is clicked, selectedMovie property in the state is updated to particular movie   
     setSelectedMovie(newSelectedMovie) {
         this.setState({
           selectedMovie: newSelectedMovie
         });
       }
+      // when user logs in, user property in the state is updated to particular user
+      onLoggedIn(user) {
+        this.setState({
+          user
+        });
+      }
+      // when user registers
+      // onRegistration(register) {
+      //   this.setState({
+      //     register,
+      //   });
+      // }
+
   render() {
-   // const { movies, selectedMovie } = this.state;
+   const { movies, selectedMovie } = this.state;
+   // if there is no user login view is rendered
+    // if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+  
+   // Blank page before movies are loaded
+   if (movies.length === 0) return <div className="main-view" />
    
-
-
   return (
     <div className="main-view">
            
