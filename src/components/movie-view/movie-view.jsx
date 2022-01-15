63eb92b63dd8f81440016f76db1c0b96d2313d04
nirlepshah <nirlepshah@gmail.com>
2022-01-14 19:55:26 -0500

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -9,25 +9,29 @@ export class MovieView extends React.Component {
 
     return (
       <div className="movie-view">
-        <div className="movie-poster">
+          <div className="movie-poster"> 
           <img src={movie.ImagePath} />
-        </div>
+         </div> <br />
         <div className="movie-title">
           <span className="label">Title: </span>
           <span className="value">{movie.Title}</span>
-        </div>
+        </div> <br />
         <div className="movie-description">
           <span className="label">Description: </span>
           <span className="value">{movie.Description}</span>
-        </div>
+        </div> <br />
         <div className="movie-genre">
           <span className="label">Genre: </span>
-          <span className="value">{movie.Genre}</span>
-        </div>
+          <span className="value">{movie.Genre.Name}</span><br />
+          <span className="label">Genre Description: </span>
+          <span className="value">{movie.Genre.Description}</span>
+        </div><br />
         <div className="movie-director">
           <span className="label">Director: </span>
-          <span className="value">{movie.Director}</span>
-        </div>
+          <span className="value">{movie.Director.Name}</span><br />
+          <span className="label">Director Bio: </span>
+          <span className="value">{movie.Director.Bio}</span>
+        </div><br />
         <button onClick={() => { onBackClick(null);}}>Back</button>
        </div>
     );
@@ -39,8 +43,7 @@ MovieView.propTypes = {
    movies: propTypes.shape({
     Title: propTypes.string,
     Description: propTypes.string,
-    Genre: propTypes.string,
-    Director: propTypes.string,
+   
   }).isRequired,
   onBackClick: propTypes.func.isRequired
 };
