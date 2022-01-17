e35a630f1a9f49d3ac1374d0cf5a8b24f5435121
nirlepshah <nirlepshah@gmail.com>
2022-01-17 18:16:24 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,25 @@
-import React from 'react'; // Import React object from react module
-import ReactDOM from 'react-dom'; // Import ReactDOM object from react-dom module
-import { MainView } from './components/main-view/main-view';
-import { MovieView } from './components/movie-view/movie-view';
+import React from "react"; // Import React object from react module
+import ReactDOM from "react-dom"; // Import ReactDOM object from react-dom module
+import { MainView } from "./components/main-view/main-view";
+import { MovieView } from "./components/movie-view/movie-view";
 
 // Importing ./index.scss`
-import './index.scss';
+import "./index.scss";
+import { Container } from "react-bootstrap";
 
-// Main component 
+// Main component
 class MyFlix extends React.Component {
   render() {
     return (
-      <MainView />
+      <Container>
+        <MainView />
+      </Container>
     );
   }
 }
 
 // Finds the root of app from index.html
-const container = document.getElementsByClassName('app-container')[0];
+const container = document.getElementsByClassName("app-container")[0];
 
 // Tells ReactDOM object to render app in the root DOM element using render method
 ReactDOM.render(React.createElement(MyFlix), container);
