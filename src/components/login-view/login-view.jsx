038d50d90df2fa4c97e6876c5a0ae615fd7b108a
nirlepshah <nirlepshah@gmail.com>
2022-01-17 18:13:46 -0500

    Add React-Bootstrap components

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,9 +1,25 @@
-import React, { useState } from 'react'; // import useState hook
-import propTypes from 'prop-types'; // import propType
+import React, { useState } from "react"; // import useState hook
+import propTypes from "prop-types"; // import propType
+//Import React Bootstrap Components
+import {
+  Form,
+  FormGroup,
+  CardGroup,
+  Card,
+  Col,
+  Button,
+  Row,
+  Container,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import "./login-view.scss";
+
 ////Login View Component
 export function LoginView(props) {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   // method to hadle submit
   const handleSubmit = (e) => {
@@ -11,26 +27,55 @@ export function LoginView(props) {
     console.log(username, password);
     props.onLoggedIn(username); // allows  user to be automatically log in
   };
+
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    // Add React BootStrap Elements
+    <div className="LoginContainer">
+      <Container fluid>
+        <Row style={{ display: "flex", justifyContent: "center" }}>
+          <Col xs={8} md={6} lg={4}>
+            <CardGroup className="LoginCard">
+              <Card>
+                <Card.Body>
+                  <Card.Title>Login</Card.Title>
+
+                  <Form>
+                    <Form.Group>
+                      <Form.Label> Username:</Form.Label>
+                      <Form.Control
+                        type="text"
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Enter Username"
+                      />
+                    </Form.Group>
+                    <br />
+                    <Form.Group>
+                      <Form.Label> Password:</Form.Label>
+                      <Form.Control
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter Password"
+                      />
+                    </Form.Group>
+                    <br />
+                    <Button
+                      variant="primary"
+                      type="submit"
+                      onClick={handleSubmit}
+                    >
+                      Submit
+                    </Button>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </CardGroup>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
-  
 }
 //propType defined for LoginView  Component
 LoginView.propTypes = {
-  user: propTypes.shape({
-    username: propTypes.string,
-    password: propTypes.string
-  }).isRequired,
   onLoggedIn: propTypes.func.isRequired,
 };
