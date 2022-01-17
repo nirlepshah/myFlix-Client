99a2a0fc162adb0fccd389488a9616a202cdd72f
nirlepshah <nirlepshah@gmail.com>
2022-01-17 18:14:11 -0500

    Add React-Bootstrap components

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,83 +1,113 @@
-import React from 'react';
- import { RegistrationView } from '../registration-view/registration-view';
-import { LoginView } from '../login-view/login-view';
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../movie-view/movie-view';
+import React from "react";
+import { RegistrationView } from "../registration-view/registration-view";
+import { LoginView } from "../login-view/login-view";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+import { Container, Row, Col } from "react-bootstrap"; // Import React Bootstrap Components
+import "./main-view.scss";
 
-import axios from 'axios';
+import axios from "axios";
 
 export class MainView extends React.Component {
-    constructor(){
-        super(); //Adding state to the MainView Component
-        this.state = {
-            // Movie Array State
-            movies:[],
-            selectedMovie: null, // Second State
-            user: null,
-            register: null
-            
+  constructor() {
+    super(); //Adding state to the MainView Component
 
-        }
-        
-    }
-    // Axios to fetch movies from server API /movies endpoint    
-componentDidMount(){
-axios.get('https://mymovieapp08.herokuapp.com/movies')
-.then(response => {
-  this.setState({
-    movies: response.data
-  })
-})
-.catch(error => {
-  console.log(error)
-})
-    }
-    
-    // When movie is clicked, selectedMovie property in the state is updated to particular movie   
-    setSelectedMovie(newSelectedMovie) {
+    this.state = {
+      // Movie Array State
+      movies: [],
+      selectedMovie: null, // Second State
+      user: null,
+      register: null,
+    };
+  }
+  // Axios to fetch movies from server API /movies endpoint
+  componentDidMount() {
+    axios
+      .get("https://mymovieapp08.herokuapp.com/movies")
+      .then((response) => {
         this.setState({
-          selectedMovie: newSelectedMovie
+          movies: response.data,
         });
-      }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-   //  when user registers register property in the state is updated
-      onRegistration(register) {
-        this.setState({
-          register,
-        });
-      }
+  // When movie is clicked, selectedMovie property in the state is updated to particular movie
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie,
+    });
+  }
 
-    //  when user logs in, user property in the state is updated to particular user
-      onLoggedIn(user) {
-        this.setState({
-          user
-        });
-      }
-    
+  //  when user registers register property in the state is updated
+  onRegistration(register) {
+    this.setState({
+      register,
+    });
+  }
+
+  //  when user logs in, user property in the state is updated to particular user
+  onLoggedIn(user) {
+    this.setState({
+      user,
+    });
+  }
 
   render() {
-   const { movies, selectedMovie, user, register } = this.state;
-   
-   //Register view is rendered 
-   if (!register) return (<RegistrationView onRegistration={register => this.onRegistration(register)}/>);
+    const { movies, selectedMovie, user, register } = this.state;
 
-   // if there is no user login view is rendered
-    
-   if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
-  
-   // Blank page before movies are loaded
-   if (movies.length === 0) return <div className="main-view" />
-   
-  return (
-    <div className="main-view">
-           
-           {selectedMovie
-        ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-             : movies.map(movie => (
-          <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-        ))
-      }
-    </div>
-  );
+    //Register view is rendered
+    if (!register)
+      return (
+        <RegistrationView
+          onRegistration={(register) => this.onRegistration(register)}
+        />
+      );
+
+    // if there is no user login view is rendered
+
+    if (!user)
+      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+
+    // Blank page before movies are loaded
+    if (movies.length === 0) return <div className="main-view" />;
+
+    return (
+      //Add React Bootstrap components
+      <div className="main-view">
+        <Row className="main-view justify-content-md-center">
+          {selectedMovie ? (
+            <Row className="justify-content-md-center">
+              {" "}
+              <Col md={8}>
+                {" "}
+                <MovieView
+                  movie={selectedMovie}
+                  onBackClick={(newSelectedMovie) => {
+                    this.setSelectedMovie(newSelectedMovie);
+                  }}
+                />{" "}
+              </Col>{" "}
+            </Row>
+          ) : (
+            <Row className="justify-content-md-center">
+              {movies.map((movie) => (
+                <Col md={3}>
+                  <MovieCard
+                    key={movie._id}
+                    movie={movie}
+                    onMovieClick={(movie) => {
+                      this.setSelectedMovie(movie);
+                    }}
+                  />
+                </Col>
+              ))}
+            </Row>
+          )}
+        </Row>
+      </div>
+    );
   }
 }
