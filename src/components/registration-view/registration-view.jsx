3a92e182b2dffbf808c2f3d9e2d96289f81b6d55
nirlepshah <nirlepshah@gmail.com>
2022-01-24 23:44:15 -0500

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react"; // import useState Hook
+import axios from "axios";
 import propTypes from "prop-types"; //import propTypes
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { LoginView } from "../login-view/login-view";
+import { MainView } from "../main-view/main-view";
+
 //Import React Bootstrap Components
 import {
   Form,
@@ -14,6 +20,7 @@ import {
   Navbar,
 } from "react-bootstrap";
 import "./registration-view.scss";
+import { LoginView } from "../login-view/login-view";
 
 //Registration View Component
 export function RegistrationView(props) {
@@ -21,40 +28,97 @@ export function RegistrationView(props) {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [birthday, setBirthday] = useState("");
+  //useState hook required for the validatons
+  const [values, setValues] = useState({
+    usernameErr: "",
+    passwordErr: "",
+    emailErr: "",
+  });
+
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setValues({ ...values, usernameErr: "Username is Required" });
+      isReq = false;
+    } else if (username.length < 5) {
+      setValues({
+        ...values,
+        usernameErr: "Username must be 5 characters long",
+      });
+      isReq = false;
+    }
+    if (!password) {
+      setValues({ ...values, passwordErr: "Password Required" });
+      isReq = false;
+    } else if (password.length < 6) {
+      setValues({
+        ...values,
+        passwordErr: "Password must be 6 characters long",
+      });
+      isReq = false;
+      if (!email) {
+        setValues({ ...values, emailErr: "Email required" });
+      }
+      isReq = false;
+    } else if (email.indexOf("@") === -1) {
+      setValues({ ...values, emailErr: "Email is invalid" });
+      isReq = false;
+    }
+    return isReq;
+  };
 
   //method to handle Registration
   const handleRegister = (e) => {
     e.preventDefault();
-    console.log(username, password);
-    props.onRegistration(username); //allows  user to register default
+    //  console.log(username, password);
+    const isReq = validate();
+    if (isReq) {
+      axios
+        .post("http://mymovieapp08.herokuapp.com/users", {
+          Username: username,
+          Password: password,
+          Email: email,
+          Birthday: birthday,
+        })
+        .then((response) => {
+          const data = response.data;
+          console.log(data);
+          alert("Registration successful, please login");
+          // window.open("/", "_self");
+        })
+        .catch((response) => {
+          console.response(response);
+          alert("unable to register");
+        });
+      <LoginView />;
+      // props.onRegistration(username); //allows  user to register default
+    }
   };
 
   return (
     <>
       {/* Add React Bootstrap Components  */}
       <Container fluid>
-        <Navbar fixed="top" className="registrationnav">
+        {/* <Navbar fixed="top" className="registrationnav">
           <Container>
             <Navbar.Brand href="#home">
               {" "}
               <h1> MyFlix </h1>{" "}
             </Navbar.Brand>
 
-            <Nav className="me-auto"></Nav>
+            {/* <Nav className="me-auto"></Nav>
             <Nav.Link
               className="registerLink"
               tyle={{ color: "#EEB76B" }}
               href="#action1"
             >
               Login
-            </Nav.Link>
-          </Container>
-        </Navbar>
-
+            </Nav.Link> */}
+        {/* </Container>
+        </Navbar> */}{" "}
         <div className="box"></div>
-
         <Row style={{ display: "flex", justifyContent: "center" }}>
-          <Col xs={8} md={6} lg={4}>
+          <Col xs={8} md={6} lg={6}>
             <CardGroup className="register-card">
               <Card>
                 <Card.Body>
@@ -64,16 +128,18 @@ export function RegistrationView(props) {
                     <Form.Group>
                       <Form.Label> Username:</Form.Label>
                       <Form.Control
+                        input
                         type="text"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
-                        required
+                        // minLength={6}
                         placeholder="Enter Username"
                       />
+                      {values.usernameErr && <p>{values.usernameErr}</p>}
                     </Form.Group>
-                    <Form.Text className="text-muted">
-                      *Username Required
-                    </Form.Text>
+                    {/* <Form.Text className="text-muted">
+                      *Min 5 characters Username Required
+                    </Form.Text> */}
 
                     <Form.Group>
                       <Form.Label> Password: </Form.Label>
@@ -82,10 +148,11 @@ export function RegistrationView(props) {
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        required
-                        minLength={8}
+                        // required
+                        // minLength={6}
                         placeholder="Enter Password"
                       />
+                      {values.passwordErr && <p>{values.passwordErr}</p>}
                     </Form.Group>
 
                     <Form.Group>
@@ -97,9 +164,10 @@ export function RegistrationView(props) {
                         onChange={(e) => setEmail(e.target.value)}
                         placeholder="Enter Email address"
                       />
-                      <Form.Text className="text-muted">
+                      {values.emailErr && <p>{values.emailErr}</p>}
+                      {/* <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
-                      </Form.Text>
+                      </Form.Text> */}
                     </Form.Group>
 
                     <Form.Group>
@@ -109,7 +177,7 @@ export function RegistrationView(props) {
                         type="date"
                         value={birthday}
                         onChange={(e) => setBirthday(e.target.value)}
-                        required
+                        // required
                       />
                     </Form.Group>
 
@@ -122,6 +190,10 @@ export function RegistrationView(props) {
                     >
                       Register
                     </Button>
+                    <p></p>
+                    <p>
+                      Already registered <Link to={"/"}>Sign In</Link> here{" "}
+                    </p>
                   </Form>
                 </Card.Body>
               </Card>
