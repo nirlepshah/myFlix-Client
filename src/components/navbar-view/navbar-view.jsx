62bd5cf7fc2b6b0f6e30d46011e74af51b01399e
nirlepshah <nirlepshah@gmail.com>
2022-01-26 21:39:20 -0500

    Update navbar-view.jsx

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -9,56 +9,10 @@ import {
   Button,
 } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-
-// export class NavBar extends React.Component {
-//   constructor() {
-//     super();
-
-//     this.state = {};
-//   }
-
-//   onLoggedOut() {
-//     localStorage.clear();
-//     window.open("/", "_self");
-//   }
-
-//   render() {
-//     const { user } = this.props;
-//     const movies = `/`;
-// const profile = `/users/${user}`;
-
-//     if (!user) return null;
-
-//     return (
-//       <Navbar bg="dark" collapseOnSelect fixed="top" expand="lg" variant="dark">
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-//         <Navbar.Collapse id="responsive-navbar-nav">
-//           <Nav className="ml-auto">
-//             <NavLink to={movies} className="link-text">
-//               Movies
-//             </NavLink>
-
-//             <NavLink to={profile} className="link-text">
-//               Profile
-//             </NavLink>
-
-//             <NavLink to="/" onClick={this.onLoggedOut}>
-//               Log Out
-//             </NavLink>
-//           </Nav>
-//           <Form>
-//             <FormControl type="text" placeholder="Search" />
-//           </Form>
-//         </Navbar.Collapse>
-//       </Navbar>
-//     );
-//   }
-// }
-
-// export default NavBar;
+import "./navbar-view.scss";
 
 export function NavBar({ user }) {
+  const Username = localStorage.getItem("user");
   const isAuth = () => {
     if (typeof window == "undefined") {
       return false;
@@ -86,8 +40,14 @@ export function NavBar({ user }) {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="m1-auto">
             {isAuth() && <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>}
+
+            {!isAuth() && <Nav.Link href="/">Sign-in</Nav.Link>}
+            {!isAuth() && <Nav.Link href="/register">Sing-up</Nav.Link>}
+            <Nav.Link href={`/users/${Username}`}>Profile</Nav.Link>
             {!isAuth() && (
               <Button
+                className="btn"
+                variant="danger"
                 onClick={() => {
                   localStorage.clear();
                   window.open("/", "self");
@@ -96,10 +56,7 @@ export function NavBar({ user }) {
                 Logout
               </Button>
             )}
-            {!isAuth() && <Nav.Link href="/">Sign-in</Nav.Link>}
-            {!isAuth() && <Nav.Link href="/register">Sing-up</Nav.Link>}
           </Nav>
-          <Nav.Link href="/profileview">Profile</Nav.Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
