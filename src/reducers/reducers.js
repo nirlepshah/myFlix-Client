8875c65216473c7e2cb32c5c2d6dab84020d500c
nirlepshah <nirlepshah@gmail.com>
2022-01-30 20:51:45 -0500

    Create reducers.js

---
+++ src/reducers/reducers.js
@@ -0,0 +1,33 @@
+import { combineReducers } from 'redux';
+
+import { SET_FILTER, SET_MOVIES } from '../actions/actions';
+
+function visibilityFilter(state = '', action) {
+  switch (action.type) {
+    case SET_FILTER:
+        console.log('SET_FILTER reducer reached')
+    return action.value;
+    default:
+      return state;
+  }
+}
+
+function movies(state = [], action) {
+  switch (action.type) {
+    case SET_MOVIES:
+        console.log('SET_MOVIES reducer reached')
+      return action.value;
+    default:
+      return state;
+  }
+}
+
+function moviesApp(state = {}, action) {
+  return {
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+    movies: movies(state.movies, action)
+  }
+}
+
+
+export default moviesApp;
