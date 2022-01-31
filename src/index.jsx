4aa1bbeb83d051e0de1b4705fe2698da460e3837
nirlepshah <nirlepshah@gmail.com>
2022-01-30 20:51:37 -0500

    Update index.jsx adding redux store

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,29 @@
 import React from "react"; // Import React object from react module
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ReactDOM from "react-dom"; // Import ReactDOM object from react-dom module
-import { MainView } from "./components/main-view/main-view";
+import { createStore } from "redux";
+import { Provider } from "react";
+import { devToolsEnhancer } from "redux-devtools-extension";
+
+import moviesApp from "./reducers/reducers";
+import MainView from "./components/main-view/main-view";
 import { MovieView } from "./components/movie-view/movie-view";
 
 // Importing ./index.scss`
 import "./index.scss";
 import { Container } from "react-bootstrap";
+import { Provider } from "react-redux";
+const store = createStore(moviesApp, devToolsEnhancer());
 
 // Main component
 class MyFlix extends React.Component {
   render() {
     return (
-      <Container>
-        <MainView />
-      </Container>
+      <Provider store={store}>
+        <Container>
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
